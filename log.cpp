0aa6345f8849543db0a1e4a20f7267fbf44d4efb
akashgangil <akash.gangil@outlook.com>
2014-01-20 03:35:44 -0500

    log file generation

--- log.cpp
+++ log.cpp
@@ -3,15 +3,30 @@
  **   Description: Logging functionality, always flushes.
  **/
 #include <stdio.h>
+#include <string.h>
+#include <time.h>  
 #include "params.h"
 #include "log.h"
 
 static int openFile = 0;
 FILE *fileDescriptor = NULL;
-const char *logLocation = "./logFile";
+
+char buf[80];
+
+
+char* currentDateTime() {
+    time_t     now = time(0);
+    struct tm  tstruct;
+    tstruct = *localtime(&now);
+    strftime(buf, sizeof(buf), "%Y-%m-%d.%X", &tstruct);
+    return buf;
+}
 
 int log_open() {
-    fileDescriptor = fopen(logLocation, "w");
+    char logFile[100];
+    sprintf(logFile, "khanlog-%s.log",currentDateTime());
+    printf("Log File Location: %s\n", logFile);
+    fileDescriptor = fopen(logFile,"w");
     
     if(fileDescriptor == NULL) {
         printf("Cannot open log file.\n");
@@ -28,3 +43,4 @@ void log_msg(const char *msg) {
     fprintf(fileDescriptor, "%s\n",msg);
     fflush(fileDescriptor);
 }
+
